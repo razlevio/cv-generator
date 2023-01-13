4705fe06e7ffd6fcba5b00c1f9fa3b2d880d8720
razlevio <razlevio.55@gmail.com>
2023-01-13 15:18:52 +0200

    header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+    return (
+        <div className="bg-zinc-600 p-8">
+            <h1 className="text-6xl font-black text-center text-zinc-300">CV Generator</h1>
+        </div>
+    )
+};
+
+export default Header;
